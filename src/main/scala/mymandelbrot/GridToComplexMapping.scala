3ca65f5097731e2e3cbef135f58ed93459e93b3a
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-09-04 22:00:15 +0100

    - Preconvert some values to doubles.

--- src/main/scala/mymandelbrot/GridToComplexMapping.scala
+++ src/main/scala/mymandelbrot/GridToComplexMapping.scala
@@ -10,14 +10,21 @@ case class ComplexViewPort(bottomLeft: Complex, topRight: Complex)
 
 case class GridToComplexMapping(size: GridSize, viewPort: ComplexViewPort) {
 
-  val complexWidth = viewPort.topRight.real - viewPort.bottomLeft.real
-  val complexHeight = viewPort.topRight.imag - viewPort.bottomLeft.imag
+  private val complexWidth = viewPort.topRight.real - viewPort.bottomLeft.real
+  private val complexHeight = viewPort.topRight.imag - viewPort.bottomLeft.imag
 
-  def toComplex(coordinate: GridCoordinate) = {
+  private val sizeXDouble = size.x.toDouble
+  private val sizeYDouble = size.y.toDouble
 
+  def toComplex(coordinate: GridCoordinate): Complex = {
     println(s"coordinate.x: ${coordinate.x} size.x: ${size.x} coordinate.x: ${coordinate.x}")
-    val complexX = ((complexWidth / size.x.toDouble) * coordinate.x) + viewPort.bottomLeft.real
-    val complexY = ((complexHeight / size.y.toDouble) * coordinate.y) + viewPort.bottomLeft.imag
+
+//    def map(complexSize: Double, gridSize: Double, coordinateValue: Int, lowerViewValue: Double): Double = {
+//      ((complexSize / gridSize) * coordinateValue) + lowerViewValue
+//    }
+
+    val complexX = ((complexWidth / sizeXDouble) * coordinate.x) + viewPort.bottomLeft.real
+    val complexY = ((complexHeight / sizeYDouble) * coordinate.y) + viewPort.bottomLeft.imag
 
     Complex(complexX, complexY)
   }
