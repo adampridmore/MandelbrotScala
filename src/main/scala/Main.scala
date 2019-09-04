27702069f4cac562021b4938f486f6e592abad87
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-09-04 21:01:27 +0100

    - Introduced a coordinate class.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,34 @@
 import breeze.math.Complex
 import mymandelbrot.Mandelbrot
 
+case class GridCoordinate(x: Int, y: Int)
+
 object Main extends App {
-  val size = 100
+  val gridSize = 100
   val scale = 40d
 
-  val grid = Array.ofDim[String](size, size)
+  val graphBottom = -1d
+  val graphTop = 1d
+
+  val grid = Array.ofDim[String](gridSize, gridSize)
 
   def inSetToString(inSet: Boolean) = if (inSet) "*" else " "
 
+  def gridToComplex(coordinate: GridCoordinate) = {
+    val x = (coordinate.x.toDouble - (gridSize.toDouble / 2d)) / scale
+    val y = (coordinate.y.toDouble - (gridSize.toDouble / 2d)) / scale
+
+    Complex(x, y)
+  }
+
   (for {
-    x <- 0 until size
-    y <- 0 until size
-  } yield (x, y))
-    .map { case (x, y) => (Complex((x.toDouble - (size.toDouble / 2d)) / scale, (y.toDouble - (size.toDouble / 2d)) / scale), (x, y)) }
+    x <- 0 until gridSize
+    y <- 0 until gridSize
+  } yield GridCoordinate(x, y))
+    .map { coordinate => (gridToComplex(coordinate), coordinate)}
     .map { case (c, coord) => (Mandelbrot.inSet(c), coord) }
     .map { case (inSet, coord) => (inSetToString(inSet), coord) }
-    .foreach { case (inSet, coord) => grid(coord._2)(coord._1) = inSet }
+    .foreach { case (inSet, coord) => grid(coord.y)(coord.x) = inSet }
 
   grid.foreach { row => println(row.mkString) }
 }
