f1005d51fdd7a13cada264bd00905e9e8edd5be4
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-09-04 20:46:48 +0100

    - Moved rendering to main().

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
+import breeze.math.Complex
+import mymandelbrot.Mandelbrot
+
 object Main extends App {
-  println("Hello, World!")
+  val size = 100
+  val scale = 40d
+
+  val grid = Array.ofDim[String](size, size)
+
+  def inSetToString(inSet: Boolean) = if (inSet) "*" else " "
+
+  (for {
+    x <- 0 until size
+    y <- 0 until size
+  } yield (x, y))
+    .map { case (x, y) => (Complex((x.toDouble - (size.toDouble / 2d)) / scale, (y.toDouble - (size.toDouble / 2d)) / scale), (x, y)) }
+    .map { case (c, coord) => (Mandelbrot.inSet(c), coord) }
+    .map { case (inSet, coord) => (inSetToString(inSet), coord) }
+    .foreach { case (inSet, coord) => grid(coord._2)(coord._1) = inSet }
+
+  grid.foreach { row => println(row.mkString) }
 }
--- src/test/scala/MandelbrotTest.scala
+++ src/test/scala/MandelbrotTest.scala
@@ -66,25 +66,4 @@ class MandelbrotTest extends FunSuite {
 
     println(results)
   }
-
-  test("Set") {
-
-    val size = 100
-    val scale = 40d
-
-    val grid = Array.ofDim[String](size,size)
-
-    def inSetToString(inSet: Boolean) = if (inSet) "*" else " "
-
-    (for {
-      x <- 0 until size
-      y <- 0 until size
-    } yield (x, y))
-      .map { case (x, y) => (Complex((x.toDouble - (size.toDouble / 2d)) / scale, (y.toDouble - (size.toDouble / 2d)) / scale), (x, y)) }
-      .map { case (c, coord) => (Mandelbrot.inSet(c), coord) }
-      .map { case (inSet, coord) => (inSetToString(inSet), coord) }
-      .foreach { case (inSet, coord) => grid(coord._1)(coord._2) = inSet }
-
-    grid.foreach { row => println(row.mkString) }
-  }
 }
