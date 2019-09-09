aab88adf9daa4c6a6d592ca42713820a7a3d5916
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-09-09 20:18:39 +0100

    - Added screen resolutions  - Opens image when rendered

--- src/main/scala/org/mandelbrot/console/MainImageRender.scala
+++ src/main/scala/org/mandelbrot/console/MainImageRender.scala
@@ -12,7 +12,9 @@ object MainImageRender extends App {
 
   private def render(): Unit = {
     val view = ComplexViewPort(Complex(-2, -1), Complex(0.5, 1))
-    val gridSize = GridSize(x = 100, y = 100)
+//    val gridSize = GridSize(x = 2560, y = 1600)
+//    val gridSize = GridSize(x = 1024, y = 768)
+    val gridSize = GridSize(x = 320, y = 200)
 
     val image = new BufferedImage(gridSize.x, gridSize.y, BufferedImage.TYPE_INT_RGB)
 
@@ -24,10 +26,13 @@ object MainImageRender extends App {
       .sequence(map)
       .foreach { case (coord, color) => image.setRGB(coord.x, coord.y, color) }
 
-    val stream = new FileOutputStream("generatedImages/image_mandelbrot.png")
+    val filename = "generatedImages/image_mandelbrot.png"
+    val stream = new FileOutputStream(filename)
 
     ImageIO.write(image, "png", stream)
     stream.close()
+
+    Runtime.getRuntime.exec(s"open $filename")
   }
 
   render()
