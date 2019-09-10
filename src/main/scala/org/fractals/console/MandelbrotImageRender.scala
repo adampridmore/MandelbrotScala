e106cc1515ea7bd0863b03406bffa5f4cc5f2d6d
Adam Pridmore <adam_pridmore@hotmail.com>
2019-09-10 22:36:51 +0100

    - Support opening output in Windows as well as a Mac

--- src/main/scala/org/fractals/console/MandelbrotImageRender.scala
+++ src/main/scala/org/fractals/console/MandelbrotImageRender.scala
@@ -2,18 +2,31 @@ package org.fractals.console
 
 import java.awt.Color
 import java.awt.image.BufferedImage
-import java.io.FileOutputStream
+import java.io.{File, FileOutputStream}
 
 import breeze.math.Complex
+import com.sun.javafx.util.Utils._
 import javax.imageio.ImageIO
 import org.fractals.mandelbrot._
 
 object MandelbrotImageRender extends App {
 
+  def open(filename: String) = {
+
+    val cmd = (isWindows, isMac) match {
+      case (true, _) => Array("cmd.exe", "/c", filename)
+      case (_, true) => Array(s"open $filename")
+      case _ => throw new Exception(s"Unknown OS: ${System.getProperty("os.name")}")
+    }
+
+    Runtime.getRuntime.exec(cmd)
+  }
+
   private def render(): Unit = {
     val view = ComplexViewPort(Complex(-2, -1), Complex(0.5, 1))
-//    val gridSize = GridSize(x = 2560, y = 1600)
-//    val gridSize = GridSize(x = 1024, y = 768)
+//        val gridSize = GridSize(x = 2560 * 4, y = 1600 * 4)
+//        val gridSize = GridSize(x = 2560, y = 1600)
+    //    val gridSize = GridSize(x = 1024, y = 768)
     val gridSize = GridSize(x = 320, y = 200)
 
     val image = new BufferedImage(gridSize.x, gridSize.y, BufferedImage.TYPE_INT_RGB)
@@ -26,13 +39,15 @@ object MandelbrotImageRender extends App {
       .sequence(map)
       .foreach { case (coord, color) => image.setRGB(coord.x, coord.y, color) }
 
-    val filename = "generatedImages/image_mandelbrot.png"
+
+    val filename = s"generatedImages${File.separator}image_mandelbrot.png"
+    println(s"Filename: $filename")
     val stream = new FileOutputStream(filename)
 
     ImageIO.write(image, "png", stream)
     stream.close()
 
-    Runtime.getRuntime.exec(s"open $filename")
+    open(filename)
   }
 
   render()
