a9ebadd40ee8a1fddc1ddf773874786fc99120ba
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-10-18 23:07:52 +0100

    - Changed collection to be a lazy wait for the collection.  - Not sure if this is a good thing to do...

--- src/main/scala/org/fractals/repository/CrudMongoCollection.scala
+++ src/main/scala/org/fractals/repository/CrudMongoCollection.scala
@@ -1,12 +1,20 @@
 package org.fractals.repository
 
 import reactivemongo.api.collections.bson.BSONCollection
-
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 trait CrudMongoCollection extends CrudMongoDb {
-  def collection: Future[BSONCollection] =
-    db.map(_.collection(collectionName()))
 
-  def collectionName(): String
+  private def collectionF: Future[BSONCollection] =
+    db.map(_.collection(collectionName))
+
+  // TODO: Not sure about this. await?
+  private lazy val collectionAwait: BSONCollection = {
+    Await.result(db.map(_.collection(collectionName)), 10 seconds)
+  }
+
+  def collection = collectionAwait
+
+  def collectionName: String
 }
--- src/main/scala/org/fractals/repository/CrudMongoRepository.scala
+++ src/main/scala/org/fractals/repository/CrudMongoRepository.scala
@@ -4,9 +4,11 @@ import reactivemongo.bson.{BSONDocumentReader, BSONDocumentWriter, document}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait CrudMongoRepository[T] extends CrudMongoCollection  {
+trait CrudMongoRepository[T] extends CrudMongoCollection {
   implicit val ec: ExecutionContext
+
   implicit def imageWriter: BSONDocumentWriter[T]
+
   implicit def imageReader: BSONDocumentReader[T]
 
   def collectionName() = "mandelbrotImages2"
@@ -14,16 +16,15 @@ trait CrudMongoRepository[T] extends CrudMongoCollection  {
   def fetchById(id: String): Future[Option[T]] = {
     val query = document("id" -> id)
 
-    for {
-      coll <- collection
-      doc <- coll.find(query, projection = None).one
-    } yield doc
+    collection
+      .find(query, projection = None)
+      .one
   }
 
   def save(document: T): Future[Unit] = {
-    for {
-      coll <- collection
-      _ <- coll.insert(ordered = false).one(document)
-    } yield Unit
+    collection
+      .insert(ordered = false)
+      .one(document)
+      .map(_=>Unit)
   }
 }
